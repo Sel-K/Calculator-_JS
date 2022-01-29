71b8181932cd776c03b570267f86a0fa6528beda
Elena Khen <selena_hen@mail.ru>
2022-01-29 21:48:36 +0300

    объявлены переменные и вывод в консоль, алерт

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let title;
+let screen;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+alert('Hello, my JS!');
+
+let a = 'GitBash ';
+let b = 'is killing me!';
+console.log(a + b);
