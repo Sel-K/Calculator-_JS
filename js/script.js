bdd6c6e2ce98a8104cfa8b2990f3a96aedd159ad
Elena Khen <selena_hen@mail.ru>
2022-01-29 23:13:08 +0300

    заданы значения переменным из lesson1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
-let hello = 'Привет';
-const b = 'Bay';
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+title = 'Calculator_JS';
+screens = 'Simple, Complex, Interactive';
+screenPrice = 13;
+rollback = 77;
+fullPrice = 65431385797865445654n;
+adaptive = true;
+
+
+alert('Hello, my JS!');
+
+let a = 'GitBash ';
+let b = 'is killing me!';
+console.log(a + b);
