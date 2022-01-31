e66ec2339f7353d0de79733081d0544db6390870
Elena Khen <selena_hen@mail.ru>
2022-01-31 22:09:35 +0300

    Удалены лишние строки, исправлено написание значений переменных

--- js/script.js
+++ js/script.js
@@ -8,11 +8,11 @@ let adaptive = true;
 console.log(typeof 'Calculator_JS');
 console.log(typeof 65431385797865445654n);
 console.log(typeof true);
-console.log(screens.length);
+console.log(screens.length); //показывает длинну строки
 console.log('Стоимость вёрстки экранов ' + screenPrice + ' рублей/ долларов/гривен/юани');
 console.log('Стоимость разработки сайта ' + fullPrice + ' рублей/ долларов/гривен/юани');
-console.log(screens.toLowerCase().split(' '));
-console.log('Процент отката посреднику за работу ' + Number(fullPrice) * (rollback/100));
+console.log(screens.toLowerCase().split(' ')); //toLowerCase - все буквы в нижнем регистре, .split - разбивает на массив строку//
+console.log('Процент отката посреднику за работу ' + Number(fullPrice) * (rollback/100)); //сначала приводим в одинаковое значение обычное число(rollback) и bigInt(fullPrice) с помощью Number
 
 alert('Hello, my JS!');
 
