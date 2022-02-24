abf1a45b828f58893778519d4683ddecd651c7a0
Elena Khen <selena_hen@mail.ru>
2022-02-24 14:03:42 +0300

    Наведен порядок в части выводов в консоль

--- js/script.js
+++ js/script.js
@@ -3,50 +3,43 @@
 let title = prompt('Как называется ваш проект?'); //ранее указано 'Calculator_JS';
 let screens = prompt('Какие типы экранов нужно разработать?', 'Simple, Complex, Interactive');
 let screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
-let rollback = 77;
-// 6 пункт
 let adaptiv = confirm('Нужен ли адаптив на сайте?');
+
+let rollback = 77;
+let allServicePrices;
+let fullPrice;
+let toIntermediary;//% отката посреднику
+let servicePercentPrice;
+
+
 // 7 пункт
 let service1 = +prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice1 = +prompt('Сколько это будет стоить?');
 let service2 = +prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice2 = +prompt('Сколько это будет стоить?');
-// 8 пункт
-let fullPrice;
-// 9 пункт
-let toIntermediary;//% отката посреднику
-let servicePercentPrice;
-let allServicePrices;
+
 
 const showTypeOff = function (variable) {
     console.log(variable, typeof variable);
 }
-
-
 // к 4 уроку пункт 1
-
 const getAllServicePrices = function (a, b) {
     return (a + b);
 }
-
 
 function getFullPrice(a, b) {
     return a + b;
 }
 
-
-
 function getServicePercentPrices(a, b) {
     return a - b;
 }
-//toIntermediary = fullPrice * (rollback / 100);
 
 function getTitle(title) {
     
         title = title.trim();
         return title[0].toUpperCase() + title.slice(1).toLowerCase();
 }
-
 
 //10 пункт
 const getRollbackMessage = function (price) {
@@ -65,10 +58,16 @@ const getRollbackMessage = function (price) {
 showTypeOff(title);
 showTypeOff(screenPrice);
 showTypeOff(adaptiv);
+
+allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+fullPrice = getFullPrice(screenPrice, allServicePrices);
+servicePercentPrice = getServicePercentPrices(fullPrice, toIntermediary = fullPrice * (rollback / 100));
+title = getTitle(title);
+
 console.log(title = getTitle(title));
 console.log(screens.toLowerCase().split(' ')); //toLowerCase - все буквы в нижнем регистре, .split - разбивает на массив строку//
-console.log(allServicePrices = getAllServicePrices(servicePrice1, servicePrice2));
-console.log(fullPrice = getFullPrice(screenPrice, allServicePrices));
-console.log(servicePercentPrice = getServicePercentPrices(fullPrice, toIntermediary = fullPrice * (rollback / 100)));
+console.log(allServicePrices);
+console.log(fullPrice);
+console.log(servicePercentPrice);
 console.log(getRollbackMessage(fullPrice));
 console.log(toIntermediary);
